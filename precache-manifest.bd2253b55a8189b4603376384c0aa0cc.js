9fdf01812337838aa2092fc9031261c949065f40
naufaliqbal <naufalopak@gmail.com>
2019-10-14 15:06:08 +0700

    service worker

---
+++ precache-manifest.bd2253b55a8189b4603376384c0aa0cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0532b087c12b7adfd464aa45f4fb7df9",
+    "url": "192.png"
+  },
+  {
+    "revision": "153bf2365e68971805e6ed165acfe58b",
+    "url": "512.png"
+  },
+  {
+    "revision": "4481a77e859d98351bb5a39cac359d6b",
+    "url": "index.html"
+  },
+  {
+    "revision": "4c99ebddf15ec45a4ef9",
+    "url": "main.js"
+  },
+  {
+    "revision": "769d5af9a803ee435c1b6e17d26fa245",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "da7286ab6c292b95cba4",
+    "url": "runtime~main.js"
+  },
+  {
+    "revision": "ff7c6a09bf15bb109226",
+    "url": "vendors~main.js"
+  }
+]);
